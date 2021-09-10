b39fdf3b94abbe289ec493a0139cfab20a5f0f30
Bm Raihanuzzaman <bmraihanuzzaman@Bms-MacBook-Pro.fios-router.home>
2021-09-09 23:52:14 -0400

    first commit

---
+++ src/HelloWorld.java
@@ -0,0 +1,6 @@
+public class HelloWorld {
+    public static void main(String[] args) {
+
+int age = 59;
+if ("age" <=69){System.out.println;}"(yes you can claim)
+    else("age">=69){System.out.print.)"{you can  not claim}
